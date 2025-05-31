a71f85f38f8e50446580978eb1467cb79e0cbdcd
HARI_IITM <mech.hnath@gmail.com>
2025-05-31 10:31:40 +0530

    Day_2

---
+++ Day_2.py
@@ -0,0 +1,7 @@
+## Program to calculate area of a Triangle
+
+length = int(input("Enter the length of a triangle (in cm) : "))
+breadth  = int(input("Enter the breadth of a triangle (in cm) : "))
+Area = 1/2* length * breadth
+
+print(f'The Area of triangle with (length : {length} cm and breadth : {breadth} cm) is : {Area} squared cm')
